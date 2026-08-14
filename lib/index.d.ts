114f75999d8d477951cbd91ee4d1e0f5360d6627
bugmaker2 <qby_qiubaiyuan@qq.com>
2026-08-14 12:17:49 +0800

    Initial TypeScript plugin template

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+//#region src/index.d.ts
+/** Cordis plugin name. */
+declare const name = "dsh-plugin-template";
+/** Start the plugin when Harness loads it. */
+declare function apply(): void;
+//#endregion
+export { apply, name };
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/** Cordis plugin name. */
+export const name = 'dsh-plugin-template'
+
+/** Start the plugin when Harness loads it. */
+export function apply(): void {
+  console.log('[dsh-plugin-template] hello world')
+}
